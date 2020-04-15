91f0219455e7f5070a675d92cc78bf044d2398e6
Aaron <roycet000@gmail.com>
2020-04-15 16:33:41 -0700

    Add files via upload

---
+++ src/JavaWeek03Hw.java
@@ -0,0 +1,34 @@
+package week03Hw;
+
+public class JavaWeek03Hw {
+
+	public static void main(String[] args) {   
+		
+		/*
+		 * 1.	Create an array of int called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+			a.	Programmatically subtract the value of the first element in the array from the value in the last element 
+			of the array (do not type ANY numbers in the operation, ages[7] – ages[0] is not allowed). Print the result to the console.
+			b.	Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
+			c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+		
+	}
+	
+	
+}
